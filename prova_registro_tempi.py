7ca84cdd3e4b373d4b61fa82e8c2e72f9eb25212
abx88 <125708116+abx88@users.noreply.github.com>
2023-09-22 00:09:10 +0200

    Update prova_registro_tempi.py

--- prova_registro_tempi.py
+++ prova_registro_tempi.py
@@ -27,18 +27,16 @@ if 'lista_lav' not in st.session_state:
 
 if 'contatore' not in st.session_state:
     st.session_state.contatore = 0
-    
-    
-
-    
+      
 def on_text_input_change(text):
     ora = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     st.session_state.contatore+=1
     n=st.session_state.contatore
     st.session_state.lista_lav.append((n, text, ora))
 
+df = pd.DataFrame[1,2,3]
 
-
+st.data_editor(df)
  
    
     
