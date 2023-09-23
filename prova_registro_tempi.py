59a762cc3571eef508e6a26850065f0b606a59e1
abx88 <125708116+abx88@users.noreply.github.com>
2023-09-23 15:42:18 +0200

    Update prova_registro_tempi.py

--- prova_registro_tempi.py
+++ prova_registro_tempi.py
@@ -8,18 +8,20 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
+
+def on_change_add(input_text):
+    ora = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    text = input_text
+    st.session_state.lista.append((text, ora))
+    del st.session_state.stringa_lav
+
+
 if 'stringa_lav' not in st.session_state:
     st.session_state.stringa_lav = ""
     
 if 'lista' not in st.session_state:
     st.session_state.lista = []
 
-
-def on_change_add(input_text):
-    ora = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    text = input_text
-    st.session_state.lista.append((text, ora))
-    
 input_lavorazione = st.text_input("stringa lavorazione", key='stringa_lav')
 
 if input_lavorazione:
