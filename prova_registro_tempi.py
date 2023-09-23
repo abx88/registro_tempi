edeb2380379df7b6d7c45945921fe250791b3d2e
abx88 <125708116+abx88@users.noreply.github.com>
2023-09-23 16:10:35 +0200

    Update prova_registro_tempi.py

--- prova_registro_tempi.py
+++ prova_registro_tempi.py
@@ -15,24 +15,26 @@ if reset_session_state:
 
 def on_change_add(text):
     ora = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    st.session_state.lista.append((text, ora))
+    st.session_state.contatore+=1
+    n=st.session_state.contatore
+    st.session_state.lista.append((n, text, ora))
     del st.session_state.stringa_lav
     
     
 if 'stringa_lav' not in st.session_state:
     st.session_state.stringa_lav = ""
     
-if 'lista' not in st.session_state:
+if 'lista_lav' not in st.session_state:
     st.session_state.lista = []
 
-
+if 'contatore' not in st.session_state:
+    st.session_state.contatore = 0
+    
 input_lavorazione = st.text_input("stringa lavorazione", key='stringa_lav')
 
 if input_lavorazione:
     on_change_add(input_lavorazione)
-   
     st.rerun()
 
-st.dataframe(st.session_state.lista, use_container_width=True) 
-#del st.session_state.stringa_lav
+st.dataframe(st.session_state.lista_lav, use_container_width=True) 
 
