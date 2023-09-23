45cb798581b0794cf0b519069d05e5a0b1d3240e
abx88 <125708116+abx88@users.noreply.github.com>
2023-09-23 15:40:40 +0200

    Update prova_registro_tempi.py

--- prova_registro_tempi.py
+++ prova_registro_tempi.py
@@ -15,13 +15,14 @@ if 'lista' not in st.session_state:
     st.session_state.lista = []
 
 
-def on_change_add(text):
+def on_change_add(input_text):
     ora = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    text = input_text
     st.session_state.lista_lav.append((text, ora))
     
 input_lavorazione = st.text_input("stringa lavorazione", key='stringa_lav')
 
 if input_lavorazione:
-    on_text_input_change(input_lavorazione)
+    on_change_add(input_lavorazione)
     
 st.dataframe(st.session_state.lista, use_container_width=True) 
